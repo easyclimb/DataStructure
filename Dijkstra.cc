8f842c3fb21eaeb164e93bbd38066c1975a898a8
easyclimb <904928537@qq.com>
2015-09-04 09:54:39 +0800

    Update Dijkstra.cc

--- Dijkstra.cc
+++ Dijkstra.cc
@@ -5,7 +5,7 @@ using namespace std;
 
 #define n 9
 
-void Dijkstra(int graph[n][n], vertex_t src) {
+void Dijkstra(int graph[n][n], int src) {
     int minDis[n], previous[n];
     bool final[n];
     for(int i = 0; i < n; i++) {   //initialization
@@ -24,37 +24,80 @@ void Dijkstra(int graph[n][n], vertex_t src) {
         }
         final[minIdx] = true;
         for(int j = 0; j < n; j++) {
-            if(!final[j] && graph[minIdx][j] && minDis[minIdx] + graph[minIdx][j] < minDis[j])
+            if(!final[j] && graph[minIdx][j] && minDis[minIdx] + graph[minIdx][j] < minDis[j]) {
                 minDis[j] = minDis[minIdx] + graph[minIdx][j];
+                previous[j] = minIdx;
+            }
         }
     }
     for(int i = 0; i < n; i++)
         cout << "To " << i << " " << minDis[i] << endl;
+    for(int i = 0; i < n; i++) {
+        stack<int> stk;
+        int j = i;
+        stk.push(j);
+        while(j != src) {
+            stk.push(previous[j]);
+            j = previous[j];
+        }
+        while(!stk.empty()) {
+            cout << stk.top() << ' ';
+            stk.pop();
+        }
+        cout << endl;
+    }
 }
 
 /************************用邻接表 Adjacency List表示图**********************/
 
-struct neighbour {
+struct neighbor {
     int dst;
     int weight;
-    neighbour(int d, int w) : dst(d), weight(w) {}
+    neighbor(int d, int w) : dst(d), weight(w) {}
 };
 
-typedef vector<vector<neighbour>> Graph;
+typedef vector<vector<neighbor>> Graph;
 
 void Dijkstra2(Graph graph, int src) {
     int n = graph.size();
-    bool final[n];
     int minDis[n], previous[n];
+    bool final[n];
     for(int i = 0; i < n; i++) {
-        final[i] = false;
         minDis[i] = INT_MAX;
         previous[i] = -1;
+        final[i] = false;
     }
     minDis[src] = 0;
-    set<pair<int,int>> que;
-    que.insert({minDis[src], src});
-    while(!que.empty()) {
-        
+    for(int i = 0; i < n; i++) {
+        int minVal = INT_MAX, minIdx = -1;
+        for(int j = 0; j < n; j++) {
+            if(!final[j] && minDis[j] < minVal) {
+                minVal = minDis[j];
+                minIdx = j;
+            }
+        }
+        final[minIdx] = true;
+        vector<neighbor> neighbors = graph[minIdx];
+        for(auto nei : neighbors) {
+            if(!final(nei.dst) && minVal + nei.weight < minDis[nei.dst]) {
+                minDis[nei.dst] = minVal + nei.weight;
+                previous[nei.dst] = minIdx;
+            }
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        cout << "Path:" << endl;
+        stack<int> stk;
+        stk.push(i);
+        int j = i;
+        while(j != src) {
+            stk.push(previous[j]);
+            j = previous[j];
+        }
+        while(!stk.empty()) {
+            cout << stk.top() << ' ';
+            stk.pop();
+        }
+        cout << "Dis: " << minDis[i] << endl;
     }
 }
