cfaae4c6171b7fdcd0833f66623544ac0899ddc4
easyclimb <904928537@qq.com>
2015-09-13 13:38:47 +0800

    Create Insertion_Sort.cc

---
+++ Insertion_Sort.cc
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void insertionSort(int arr[], int n) {
+        int key ,
+        for(int i = 1; i < n; i++) {
+            key = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] >= key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+    //Time : O( n^2)
+    //Space : O(1)
+    //Boundary Cases : insertion sort takes maximum time to sort if elements are sorted in reverse order
+    //And it takes minimum time (O(n)) when elements are already sorted
+    //Stable: yes
+    //Uses: Insertion sort is used when number of elements is small. It can also be used when input array is almost sorted
+    //only few elements are misplaced in complete big array
+    
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode dummy(INT_MIN), *cur = &dummy;
+        dummy.next = head;
+        while(cur->next) {
+            if(
+        }
+    }
+};
