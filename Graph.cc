a8fd528325b36510da49d21d1c7bd69a4a752851
easyclimb <904928537@qq.com>
2015-09-17 17:01:48 +0800

    Create Graph.cc

---
+++ Graph.cc
@@ -0,0 +1,109 @@
+//DFS for graph
+//Time Complexity : O(V + E), V :  Number of Vertices, E : Number of Edges
+//Space Complexity : O(V) --> visited boolean array
+
+//topological Sort
+//Time Complexity ：O(V + E)
+//Space Complexity : O(V) --> visited, stack
+
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+//a directed graph using adjacent lists representation
+class Graph {
+public:
+	Graph(int V);   //constructor
+	void addEdge(int v, int w);   //add an adge to graph
+	void DFS(int v);    //DFS traversal for the vertices reachable from v
+	void DFS();     //print DFS traversal for the complete graph
+	vector<int> topologicalSort();
+private:
+	int V;   //No. of vertices
+	vector<int> *adj;   //a vector containing adjacent lists
+	void DFSRe(int v, bool visited[]);   //function usded by DFS
+	void topologicalSortRe(int v, bool visited[], stack<int>& stk);
+};
+
+Graph::Graph(int V) {
+	this->V = V;
+	adj = new vector<int>[V];
+}
+
+void Graph::addEdge(int v, int w) {
+	adj[v].push_back(w);
+}
+
+void Graph::DFSRe(int v, bool visited[]) {
+	visited[v] = true;
+	cout << v << " ";
+	for (auto e : adj[v])
+		if (!visited[e])
+			DFSRe(e, visited);
+}
+
+void Graph::DFS(int v) {
+	bool *visited = new bool[V];
+	for (int i = 0; i < V; i++)
+		visited[i] = false;
+	DFSRe(v, visited);
+}
+
+void Graph::DFS() {
+	bool *visited = new bool[V];
+	for (int i = 0; i < V; i++)
+		visited[i] = false;
+	for (int i = 0; i < V; i++)
+		if (!visited[i])
+			DFSRe(i, visited);
+}
+
+void Graph::topologicalSortRe(int v, bool visited[], stack<int>& stk) {
+	visited[v] = true;
+	for (auto e : adj[v])
+		if (!visited[e])
+			topologicalSortRe(e, visited, stk);
+	stk.push(v);
+}
+
+vector<int> Graph::topologicalSort() {
+	bool *visited = new bool[V];
+	for (int i = 0; i < V; i++)
+		visited[i] = false;
+	stack<int> stk;
+	vector<int> ret;
+	for (int i = 0; i < V; i++) {
+		if (!visited[i])
+			topologicalSortRe(i, visited, stk);
+	}
+	while (!stk.empty()) {
+		ret.push_back(stk.top());
+		stk.pop();
+	}
+	return ret;
+};
+
+void show(vector<int>& vec) {
+	for (auto v : vec)
+		cout << v << " ";
+	cout << endl;
+}
+
+int main()
+{
+	Graph g(6);
+	g.addEdge(4, 0);
+	g.addEdge(4, 1);
+	g.addEdge(5, 0);
+	g.addEdge(5, 2);
+	g.addEdge(2, 3);
+	g.addEdge(3, 1);
+	vector<int> ret;
+	ret = g.topologicalSort();
+	show(ret);
+	getchar();
+	return 0;
+}
+
