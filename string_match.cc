d99e519f667f3a5ab70185a76ad8b38d5722bef0
easyclimb <904928537@qq.com>
2015-09-18 21:24:22 +0800

    Create string_match.cc

---
+++ string_match.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int findIndex(string s, string p) {
+    int len1 = s.size(), len2 = p.size();
+    int i = 0; j < 0;
+    while(i < len1 && j < len2) {
+        if(s[i] == p[j]) {
+            i++;
+            j++;
+        }
+        else {
+            i = i - j + 1;
+            j = 0;
+        }
+    }
+    if(j == len2) return i - j;
+    return -1;
+}
+
+int main()
+{
+    cout << findIndex("whatis", "is") << endl;
+    return 0;
+}
