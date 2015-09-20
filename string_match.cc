83489fb12493959df409eb05c8a46cf708e4718f
easyclimb <904928537@qq.com>
2015-09-20 10:30:52 +0800

    Update string_match.cc

--- string_match.cc
+++ string_match.cc
@@ -3,10 +3,11 @@
 
 using namespace std;
 
+//Solution : two pointers
 //Time: O(m*n)
 int findIndex(string s, string p) {
     int len1 = s.size(), len2 = p.size();
-    int i = 0; j < 0;
+    int i = 0; j = 0;
     while(i < len1 && j < len2) {
         if(s[i] == p[j]) {
             i++;
@@ -21,8 +22,38 @@ int findIndex(string s, string p) {
     return -1;
 }
 
+
+//Solution 2 : KMP
+int findIndex2(string s, string p) {
+    int i = 0, j = 0;
+    int len1 = s.size(), len2 = p.size();
+    vector<int> next(len2);
+    get_next(p, next);
+    while(i < len1 && j < len2) {
+        if(j == -1 || s[i] == p[j]) {
+            i++;
+            j++;
+        }
+        else
+            j = next[j];
+    }
+    if(j == len2) return i - j;
+    return -1;
+}
+
+void get_next(string& p, vector<int>& next) {
+    int n = next.size(), j = 0, k = -1;
+    next[0] = -1;
+    while(k < n - 1) {
+        if(k == -1 || p[j] == p[k])
+            next[j++] = k++;
+        else
+            k = next[k];
+    }
+}
+
 int main()
 {
-    cout << findIndex("whatis", "is") << endl;
+    cout << findIndex2("whatis", "is") << endl;
     return 0;
 }
